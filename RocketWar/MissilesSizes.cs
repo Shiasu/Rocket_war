bca2dfafdf9d12de1ac442ce64bc104878b60322
Shiasu <vovan96mir@mail.ru>
2020-06-12 13:48:47 +0300

    Enums of sizes file added

---
+++ RocketWar/MissilesSizes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RocketWar
+{
+    public enum MissilesSizes
+    {
+        tiny = 5,
+        light = 10,
+        medium = 20,
+        heavy = 30,
+        large = 50
+    }
+}
